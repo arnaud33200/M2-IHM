a3cf64481bfc91ca014ecc015ec50e90e31a0129
arnaud33200 <arnaud33200@gmail.com>
2015-02-18 09:07:09 +0100

    GSI: projet 3 fenetre final, dedicace à JONAS ;)

--- GSI/TresVentanas/TresVentanas/Form1.vb
+++ GSI/TresVentanas/TresVentanas/Form1.vb
@@ -1,6 +1,17 @@
 ﻿Public Class Form1
 
+    Public Event nextWindow()
+
     Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        RaiseEvent nextWindow()
+    End Sub
+
+    Private Sub jej(sender As System.Object, e As System.EventArgs) Handles MyBase.FormClosing
+        End
+    End Sub
+
 End Class
